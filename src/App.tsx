52b888be46c2053538a03808884daa1c689a2e67
trianomu <trianomustika@gmail.com>
2025-05-16 10:25:14 +0700

    fix app error

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import MovieDetail from './pages/MovieDetail'
-import { useEffect, useState } from 'react'
 import Navbar from './components/layout/Navbar'
 import { useTheme } from './components/context/ThemeProvider'
 
 function App() {
     const { darkMode, toggleDarkMode } = useTheme()
-  // const [darkMode, setDarkMode] = useState(() =>
-  //   document.documentElement.classList.contains("dark")
-  // )
-
-  // useEffect(() => {
-  //   if (darkMode) {
-  //     document.documentElement.classList.add("dark")
-  //   } else {
-  //     document.documentElement.classList.remove("dark")
-  //   }
-  // }, [darkMode])
-
-  // const toggleDarkMode = () => setDarkMode((prev) => !prev)
   
   return (
     <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
